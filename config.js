317179611aa3e1642ffb811d926930f538ad2295
CharlieEagle <sheehancharlie69@gmail.com>
2023-05-24 22:42:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ config.logo_show_info = true
 // Use a video for the background?
 // Otherwise it will use images
 // "true" or "false"
-config.background_use_video = true
+config.background_use_video = false
 
 // Default available videos:
 // "blue_rays.webm"
